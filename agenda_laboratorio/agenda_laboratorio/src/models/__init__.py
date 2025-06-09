02a3dd18cb7d9bf46db7e4ea527b566d21093d99
Delevedove <154928864+Delevedove@users.noreply.github.com>
2025-06-09 08:27:58 -0300

    Delete agenda_laboratorio/agenda_laboratorio directory

--- agenda_laboratorio/agenda_laboratorio/src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# Importar todos os modelos para que sejam registrados no SQLAlchemy
-from .user import User
-from .laboratorio_turma import Laboratorio, Turma
-from .agendamento import Agendamento
-from .sala import Sala, Instrutor, ReservaSala
