b61758c44e7000ad3ff971d01a23fe62c2e84023
Delevedove <154928864+Delevedove@users.noreply.github.com>
2025-07-02 21:37:00 -0300

    Configure limiter to use Redis

--- src/limiter.py
+++ src/limiter.py
@@ -3,5 +3,11 @@ Inicializa o objeto de limitacao de requisicoes.
 """
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import os
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    get_remote_address,
+    storage_uri=os.environ.get("REDIS_URL", "memory://"),
+    storage_options={"socket_connect_timeout": 30},
+    strategy="fixed-window",
+)
